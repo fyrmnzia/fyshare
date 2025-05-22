67278e84effa187d6b5cbe5e4f41a8d3982e2eb5
fyrmnzia <1rifqi.firmansyah@gmail.com>
2025-05-22 08:42:46 +0700

    feat: implement user authentication with Clerk and Prisma, add necessary database migrations and schema

--- src/app/(auth)/callback/page.tsx
+++ src/app/(auth)/callback/page.tsx
@@ -1,5 +1,20 @@
-export const dynamic = 'force-dynamic';
+import { onAuthenticateUser } from "@/actions/auth";
+import { redirect } from "next/navigation";
 
-const AuthCallbackPage = async () => {}
+export const dynamic = "force-dynamic";
+
+const AuthCallbackPage = async () => {
+  const auth = await onAuthenticateUser();
+  if (auth.status === 200 || auth.status === 201) {
+    redirect("/home");
+  } else if (
+    auth.status === 403 ||
+    auth.status === 500 ||
+    auth.status === 400
+  ) {
+    redirect("/");
+  }
+  return <div>AuthCallbackPage</div>;
+};
 
 export default AuthCallbackPage;
